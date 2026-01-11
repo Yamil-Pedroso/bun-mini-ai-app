7a9e9dececfaa1d4ac9fb29f15154a4000b9417f
Yamil Pedroso <yamirovinci@gmail.com>
2026-01-11 16:49:35 +0100

    Initial Bun mini AI app

---
+++ index.ts
@@ -0,0 +1,7 @@
+const server = Bun.serve({
+  port: process.env.PORT ?? 3000,
+  async fetch(request) {
+    return new Response("Hello, World!");
+  },
+});
+console.log(`Server running at http://localhost:${server.port}/`);
